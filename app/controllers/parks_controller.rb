bae3fc0768a996c11236ca963dbca7c92b9f9226
Forrest Reiland <you@example.com>
2020-10-30 10:29:47 -0700

    add message for delete method

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -12,17 +12,25 @@ class ParksController < ApplicationController
 
   def create
     @park = Park.create!(park_params)
-    json_response(@park)
+    json_response(@park, :created)
   end
 
   def update
     @park = Park.find(params[:id])
-    @park.update(park_params)
+    if @park.update!(park_params)
+      render status: 200, json: {
+     message: "Park details have been updated successfully."
+     }
+    end
   end
 
   def destroy
     @park = Park.find(params[:id])
-    @park.destroy
+    if @park.destroy
+      render status: 200, json: {
+        message: "delete sucessful"
+      }
+    end
   end
 
   private
@@ -30,4 +38,5 @@ class ParksController < ApplicationController
   def park_params
     params.permit(:name, :state)
   end
+
 end
