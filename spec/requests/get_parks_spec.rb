549622bedc656f61c42eca09906210445b2a9151
Forrest Reiland <you@example.com>
2020-10-30 11:20:18 -0700

    add get park spec test, test fail

---
+++ spec/requests/get_parks_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "get all parks route", :type => :request do
+  let!(:parks) ({:name => "Zion", :state :> "Utah"})
+
+
+  before { get '/parks'}
+
+  it 'returns all parks' do
+    expect(JSON.parse(response.body).size).to eq(1)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
