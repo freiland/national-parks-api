4873cfc94e38ff5196256773997b69a1826804e6
Forrest Reiland <you@example.com>
2020-10-30 09:55:54 -0700

    move json_response to response module

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -26,9 +26,6 @@ class ParksController < ApplicationController
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
 
   def park_params
     params.permit(:name, :state)
